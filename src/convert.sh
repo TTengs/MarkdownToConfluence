95e6e4db7ad2a803c51340ba6870731c8a760052
Anders Larsen <anders.l.h.larsen@live.dk>
2022-04-08 12:23:26 +0200

    yes

--- src/convert.sh
+++ src/convert.sh
@@ -10,11 +10,11 @@ BASE_PATH=$(pwd)
 
 #Conversion
 #grep -q "\`\`\`mermaid" "$BASE_PATH/$PATHNAME/index.template.md" && docker run -v "$BASE_PATH"/"$PATHNAME":/data minlag/mermaid-cli -i /data/index.template.md -o /data/index.png
-python3 src/parse_markdown.py "$PATHNAME"
+python3 /src/parse_markdown.py "$PATHNAME"
 pandoc "$DIR"/"${FILENAME}_final.md" -f markdown -t html -o "$DIR"/"${FILENAME}".html
 rm "$DIR/${FILENAME}_final.md"
 
 #Uploading
-python3 src/upload_documentation.py "$PATHNAME" "$ROOT"
+python3 /src/upload_documentation.py "$PATHNAME" "$ROOT"
 rm "$DIR/${FILENAME}.html"
 
