8875540f7eacc453bc252e34e381c8e14f44ee22
Anders Larsen <anders.l.h.larsen@live.dk>
2022-04-12 11:50:17 +0200

    changed parent name function to get root from script and not env

--- src/convert_all.sh
+++ src/convert_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Delete pages from confluence, that dont exist in the filesystem
-python3 /src/delete_content.py "~955037829" "$INPUT_FILESLOCATION"
+#python3 /src/delete_content.py "~955037829" "$INPUT_FILESLOCATION"
 
 #Convert all files and ypload
 find $INPUT_FILESLOCATION -type f -name "*.md" -not -path "$INPUT_FILESLOCATION" -print0 | xargs -0 -I file bash /src/convert.sh file
