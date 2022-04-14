f35ed3d5d209d4e34558280f9b9bfa264fe17453
TTengs <theistengs@gmail.com>
2022-04-14 19:49:46 +0200

    Test auth

--- MarkdownToConfluence/confluence/check_if_page_exists.py
+++ MarkdownToConfluence/confluence/check_if_page_exists.py
@@ -29,6 +29,7 @@ def get_page_id(title: str, spaceKey: str) -> str:
     'Authorization': authorization_string,
     'User-Agent': 'python'
     }
+    print(authorization_string)
     response = requests.request('GET', url, headers=headers)
     if(response.status_code == 200):
         results = json.loads(response.text)['results']
