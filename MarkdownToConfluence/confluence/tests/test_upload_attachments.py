75378e3602dd5ba5c9f39e31f66e92efcb37e551
Anders Larsen <anders.l.h.larsen@live.dk>
2022-04-19 17:29:05 +0200

    test

--- MarkdownToConfluence/confluence/tests/test_upload_attachments.py
+++ MarkdownToConfluence/confluence/tests/test_upload_attachments.py
@@ -2,4 +2,5 @@ import pathlib
 from MarkdownToConfluence.confluence.upload_attachments import upload_attachment
 
 def test_upload_attachment():
-    print(upload_attachment("page 3", "test image", str(pathlib.Path(__file__).parent.resolve()) + '/test.png'))
+    response = upload_attachment("page 3", "test image", str(pathlib.Path(__file__).parent.resolve()) + '/test.png')
+    assert response.status_code == 200
