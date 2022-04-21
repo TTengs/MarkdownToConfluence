7bb9ac48ea01a22f77c849c2d2658b4a778a1f43
TTengs <theistengs@gmail.com>
2022-04-21 22:41:34 +0200

    test9

--- MarkdownToConfluence/entrypoint.sh
+++ MarkdownToConfluence/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 for i in ${{ steps.getfile.outputs.files }}
-          do
-              echo $i
-          done
+do
+    echo $i
+done
+
+echo "-------"
+
+echo ${{ steps.getfile.outputs.files }}
