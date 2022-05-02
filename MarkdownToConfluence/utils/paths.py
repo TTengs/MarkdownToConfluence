698704699de1b9701c6ceef6b18f9f87ce6d4f40
Anders Larsen <anders.l.h.larsen@live.dk>
2022-05-02 11:22:46 +0200

    attachment links

--- MarkdownToConfluence/utils/paths.py
+++ MarkdownToConfluence/utils/paths.py
@@ -8,13 +8,13 @@ def get_abs_path_from_relative(relative_path: str, source_path: str, root=os.env
     relative_path = relative_path.strip()
     source_path = source_path.strip()
     abs_path = ""
-    if(os.path.isabs(relative_path)):
+    if(os.path.exists(str(os.path.realpath((os.path.join(_root, source_path, relative_path))).strip()))):
+        abs_path = (os.path.join(_root, source_path, relative_path)).strip()
+    elif(os.path.isabs(relative_path)):
         if(os.fspath(relative_path).startswith(os.getcwd())):
             abs_path = relative_path
         else:
             abs_path = os.path.join(_root, relative_path)
-    elif(os.path.exists(str(os.path.realpath((os.path.join(_root, source_path, relative_path))).strip()))):
-        abs_path = (os.path.join(_root, source_path, relative_path)).strip()
     else:
         abs_path = os.path.abspath(relative_path)
     #print(source_path, '\n', relative_path, '\n', os.path.realpath(abs_path), '\n')
@@ -27,3 +27,5 @@ def get_abs_path_from_relative(relative_path: str, source_path: str, root=os.env
     # else:
     #     abs_path = os.path.abspath(relative_path)
     return os.path.realpath(abs_path)
+
+#print(get_abs_path_from_relative('./zip_test.zip', os.path.abspath('./documentation/page 3/index.md'), './documentation'))
