83af3d99ca86c5fe2e8f768d6cc6729628a65131
Anders Larsen <anders.l.h.larsen@live.dk>
2022-05-02 12:17:00 +0200

    split auth token into username and api token

--- MarkdownToConfluence/modules/attachment_link/parse_attachment_links.py
+++ MarkdownToConfluence/modules/attachment_link/parse_attachment_links.py
@@ -16,12 +16,10 @@ def convert_md_attachment_links_to_confluence_attachment_links(line: str, md_pat
     links = re.findall(r'(!\[(?P<alt>[^\]]*)\]\((?P<filename>.*?)(?=\"|\))(\"(?P<title>.*)\")?\))', line)
     new_line = line
     for link in links:
-        print(link)
         if(not (link[2].strip().endswith('.png') or link[2].strip().endswith('.jpg'))):
             path = get_abs_path_from_relative(link[2], md_path)
             name = link[4] if link[4] != '' else basename(link[2])
             MarkdownToConfluence.globals.attachments.append((name, path))
-            print(name, path)
             new_line = new_line.replace(link[0], f'<p class=\"media-group\"><ac:structured-macro ac:name=\"view-file\" ac:schema-version=\"1\" ac:macro-id=\"ce17b5c6-cfe6-4a77-92aa-7b810863f634\"><ac:parameter ac:name=\"name\"><ri:attachment ri:filename="{name}" ri:version-at-save=\"2\" /></ac:parameter></ac:structured-macro></p><p />')
     return new_line
 
