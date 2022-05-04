3e600bd76339314b8de4b2001ea2100fbc780d72
Anders Larsen <anders.l.h.larsen@live.dk>
2022-05-04 14:01:29 +0200

    event test

--- MarkdownToConfluence/entrypoint.sh
+++ MarkdownToConfluence/entrypoint.sh
@@ -5,5 +5,6 @@ git config --global core.pager "less -FRSX"
 git fetch -q
 #git diff --name-status origin/${GITHUB_BASE_REF} './documentation'
 printenv
+chmod -x ${GITHUB_EVENT_PATH}
 ${GITHUB_EVENT_PATH} | jq -r '.before'
 ${GITHUB_EVENT_PATH} | jq -r '.after'
