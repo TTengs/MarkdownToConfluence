33ab64250ec98d013e8700316ceb20fcaf91691f
Anders Larsen <larse19@student.sdu.dk>
2022-05-04 12:51:02 +0200

    ref test

--- MarkdownToConfluence/entrypoint.sh
+++ MarkdownToConfluence/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-/usr/bin/git init
-/usr/bin/git config --global --add safe.directory /github/workspace
-/usr/bin/git config --global core.pager "less -FRSX"
-/usr/bin/git fetch
-/usr/bin/git diff --name-only origin/${GITHUB_BASE_REF} './documentation'
+git init
+git config --global --add safe.directory /github/workspace
+git config --global core.pager "less -FRSX"
+git fetch
+git diff --name-only origin/${GITHUB_BASE_REF} './documentation'
 #echo $(printenv)
