b222a5be948c571edab968e4bc924b9dcdf8fd88
Anders Larsen <anders.l.h.larsen@live.dk>
2022-05-04 14:25:41 +0200

    event test

--- MarkdownToConfluence/entrypoint.sh
+++ MarkdownToConfluence/entrypoint.sh
@@ -3,11 +3,12 @@ git init
 git config --global --add safe.directory /github/workspace
 git config --global core.pager "less -FRSX"
 git fetch -q
-git pull
 printenv
 chmod -x ${GITHUB_EVENT_PATH}
 before=$(jq .before ${GITHUB_EVENT_PATH} | tr -d '"')
-after=$(jq .after ${GITHUB_EVENT_PATH} | tr -d '"')
+after=${GITHUB_SHA} | tr -d '"'
+git fetch origin ${before} --depth=1
+#git pull
 git diff --name-status ${before}..${after} -- './documentation'
 #echo ${GITHUB_EVENT_PATH} |  python3 -c "import sys, json; print(json.load(sys.stdin)['before'])"
 #cat ${GITHUB_EVENT_PATH} |  python3 -c "import sys, json; print(json.load(sys.stdin)['after'])"
