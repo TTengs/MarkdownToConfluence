6530b5029ec631961d60b1979b024e0d73263712
Anders Larsen <anders.l.h.larsen@live.dk>
2022-05-04 14:03:58 +0200

    event test

--- MarkdownToConfluence/entrypoint.sh
+++ MarkdownToConfluence/entrypoint.sh
@@ -6,5 +6,5 @@ git fetch -q
 #git diff --name-status origin/${GITHUB_BASE_REF} './documentation'
 printenv
 chmod -x ${GITHUB_EVENT_PATH}
-${GITHUB_EVENT_PATH} | jq -r '.before'
-${GITHUB_EVENT_PATH} | jq -r '.after'
+echo ${GITHUB_EVENT_PATH} |  python3 -c "import sys, json; print(json.load(sys.stdin)['before'])"
+echo ${GITHUB_EVENT_PATH} |  python3 -c "import sys, json; print(json.load(sys.stdin)['after'])"
