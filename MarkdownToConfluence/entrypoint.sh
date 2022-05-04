493bc8f9491151b17135d14d457457a4a63286c9
TTengs <theistengs@gmail.com>
2022-05-04 13:36:56 +0200

    on_push3

--- MarkdownToConfluence/entrypoint.sh
+++ MarkdownToConfluence/entrypoint.sh
@@ -13,7 +13,8 @@ if [[ ${GITHUB_EVENT_NAME} == "pull_request" ]]; then
     res=$(git --no-pager diff --name-status origin/${GITHUB_BASE_REF} ${INPUT_FILESLOCATION})
 elif [[ ${GITHUB_EVENT_NAME} == "push" ]]; then
     echo "I push"
-    res=$(git --no-pager diff --name-status HEAD^ ${INPUT_FILESLOCATION})
+    res=$(git --no-pager diff --name-status HEAD ${INPUT_FILESLOCATION})
+    echo $res
 fi
 
 if [[ res != "" ]]; then
