bd052e3cf8c9d63696c6816c35f98e1e3bbf537e
Anders Larsen <larse19@student.sdu.dk>
2022-05-05 17:29:26 +0200

    pytest version

--- MarkdownToConfluence/confluence/tests/test_create_content.py
+++ MarkdownToConfluence/confluence/tests/test_create_content.py
@@ -8,9 +8,12 @@ def reset_confluence():
     from MarkdownToConfluence.confluence.delete_content import delete_all_pages_in_space
 
     delete_all_pages_in_space(os.environ.get('INPUT_CONFLUENCE_SPACE_KEY'))
+    k = mock.patch.dict(os.environ, {"INPUT_FILESLOCATION": test_docs})
+    k.start()
     yield
+    k.stop()
 
-@mock.patch.dict(os.environ, {"INPUT_FILESLOCATION": test_docs, })
+@mock.patch.dict(os.environ, {"INPUT_FILESLOCATION": test_docs})
 def test_create_page():
     from MarkdownToConfluence.confluence.create_content import create_page
 
