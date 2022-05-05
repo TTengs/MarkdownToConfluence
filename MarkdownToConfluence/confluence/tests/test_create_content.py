069913211d5c1486e133799821128807a550e836
Anders Larsen <larse19@student.sdu.dk>
2022-05-05 17:32:37 +0200

    pytest version

--- MarkdownToConfluence/confluence/tests/test_create_content.py
+++ MarkdownToConfluence/confluence/tests/test_create_content.py
@@ -16,7 +16,7 @@ def reset_confluence():
 @mock.patch.dict(os.environ, {"INPUT_FILESLOCATION": test_docs})
 def test_create_page():
     from MarkdownToConfluence.confluence.create_content import create_page
-
+    print(os.environ['INPUT_FILESLOCATION'])
     with open(f'{test_docs}/testfile.md', 'w') as file:
         file.write('# testfile')
     response = create_page(f'{test_docs}/testfile.md')
