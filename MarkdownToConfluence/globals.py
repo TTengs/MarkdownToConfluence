43b7ca15068ad82666fd1aa692d536560704acc5
Anders Larsen <larse19@student.sdu.dk>
2022-05-06 10:31:44 +0200

    Update content

--- MarkdownToConfluence/globals.py
+++ MarkdownToConfluence/globals.py
@@ -2,18 +2,15 @@ import json, os
 
 def init(settings_path=""):
     global attachments, settings
-    _is_init = False
-    if(not _is_init):
-        _is_init = True
-        attachments = []
-        if(settings_path==""):
-            if(os.environ.get('INPUT_FILESLOCATION') != None):
-                try:
-                    settings = json.load(open(os.path.join(os.environ.get('INPUT_FILESLOCATION'), 'settings.json')))
-                except FileNotFoundError:
-                    settings = None
-        else:
-            settings = json.load(open(settings_path))
+    attachments = []
+    if(settings_path==""):
+        if(os.environ.get('INPUT_FILESLOCATION') != None):
+            try:
+                settings = json.load(open(os.path.join(os.environ.get('INPUT_FILESLOCATION'), 'settings.json')))
+            except FileNotFoundError:
+                settings = None
+    else:
+        settings = json.load(open(settings_path))
 
 def reset():
     global attachments
