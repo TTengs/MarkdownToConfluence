783d35fb473f0ba33cfd8431c8987a614a3e73e1
Anders Larsen <larse19@student.sdu.dk>
2022-05-06 14:55:12 +0200

    maybe everything work

--- MarkdownToConfluence/confluence/__init__.py
+++ MarkdownToConfluence/confluence/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ["confluence_utils", "create_content", "delete_content", "update_content", "upload_attachments", "convert_markdown"]
 from .confluence_utils import page_exists_in_space, get_page_id, get_all_pages_in_space
 from .create_content import create_page
-from .delete_content import delete_page, delete_all_pages_in_space, delete_non_existing_pages
+from .delete_content import delete_page, delete_all_pages_in_space, delete_non_existing_descendants
 from .update_content import update_page_content
 from .upload_attachments import upload_attachment
 from .PageNotFoundError import PageNotFoundError
