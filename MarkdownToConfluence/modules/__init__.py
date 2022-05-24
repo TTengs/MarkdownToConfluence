68599130c6fd838c2ee612cd9d7a341ac9b4f092
Anders Larsen <larse19@student.sdu.dk>
2022-05-24 21:32:56 +0200

    performance test

--- MarkdownToConfluence/modules/__init__.py
+++ MarkdownToConfluence/modules/__init__.py
@@ -1,1 +1,6 @@
-#__all__ = ['mermaid', 'jira_tickets', 'attachment_link']
+__all__ = ['mermaid', 'jira_tickets', 'attachment_link', 'table_of_contents', 'trello_boards']
+from .mermaid import run
+from .attachment_link import run
+from .jira_tickets import run
+from .table_of_contents import run
+from .trello_boards import run
